f9fec6e1325b900e90889fa7b62da83cc2783281
sobhanessi <sobhanessi@gmail.com>
2022-06-15 12:39:18 +0300

    working still

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
