cf42d7e26bfccbb69d98276056df33c6a0e09fab
sobhanessi <sobhanessi@gmail.com>
2022-07-11 17:12:38 +0200

    fixed something in the page of news

--- src/components/NewsTiles.tsx
+++ src/components/NewsTiles.tsx
@@ -1,74 +1,86 @@
-import { Avatar, Container, Grid, Typography } from "@mui/material";
+import { Avatar, Card, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import news from "../database/news";
-import { FONT_FAMILY } from "../theme/theme";
+import { FONT_FAMILY, MAIN_COLOR } from "../theme/theme";
 
 const NewsTiles = () => {
+  const newsDivert = (href: string): void => {
+    // console.log(href);
+    // inja bayad redirect konam be page morede nazar.
+  };
   return (
     <>
       <Container>
         {news.map((n) => (
           <>
-            <Grid container xs={12} sx={{ mb: 4 }}>
-              <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
-                <Avatar
-                  src={n.headerImage}
-                  alt={n.title}
-                  variant="square"
+            <Card
+              sx={{ bgcolor: MAIN_COLOR }}
+              onClick={() => newsDivert(n.href)}
+            >
+              <Grid container xs={12} sx={{ mb: 4 }}>
+                <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+                  <Avatar
+                    src={n.headerImage}
+                    alt={n.title}
+                    variant="square"
+                    sx={{
+                      height: 225,
+                      width: 225,
+                      borderRadius: "25px",
+                      display: "flex",
+                      mb: 2,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  />
+                </Grid>
+                <Grid
+                  item
                   sx={{
-                    height: 225,
-                    width: 225,
-                    borderRadius: "25px",
                     display: "flex",
-                    mb: 2,
-                    justifyContent: "center",
-                    alignItems: "center",
+                    flexDirection: "column",
                   }}
-                />
+                  xl={8}
+                  lg={8}
+                  md={8}
+                  sm={8}
+                  xs={12}
+                >
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    color="#ff6266"
+                    gutterBottom
+                    sx={{ fontFamily: FONT_FAMILY }}
+                  >
+                    {n.title}
+                  </Typography>
+                  <Typography
+                    component="div"
+                    color="#f79d2e"
+                    gutterBottom
+                    sx={{ fontFamily: FONT_FAMILY }}
+                  >
+                    {n.date}
+                  </Typography>
+                  <Typography
+                    component="div"
+                    color="#2e5aeb"
+                    gutterBottom
+                    sx={{ fontFamily: FONT_FAMILY }}
+                  >
+                    Author : {n.author}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    sx={{ color: "white", fontFamily: FONT_FAMILY }}
+                  >
+                    {n.brief}
+                  </Typography>
+                </Grid>
+                <hr style={{ color: "white", width: "100%" }} />
               </Grid>
-              <Grid
-                item
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-                xl={8}
-                lg={8}
-                md={8}
-                sm={8}
-                xs={12}
-              >
-                <Typography
-                  variant="h5"
-                  component="div"
-                  color="#ff6266"
-                  gutterBottom
-                  sx={{ fontFamily: FONT_FAMILY }}
-                >
-                  {n.title}
-                </Typography>
-                <Typography
-                  component="div"
-                  color="#f79d2e"
-                  gutterBottom
-                  sx={{ fontFamily: FONT_FAMILY }}
-                >
-                  {n.date}
-                </Typography>
-                <Typography
-                  component="div"
-                  color="#2e5aeb"
-                  gutterBottom
-                  sx={{ fontFamily: FONT_FAMILY }}
-                >
-                  Author : {n.author}
-                </Typography>
-                <Typography gutterBottom sx={{ color: "white" }}>
-                  {n.brief}
-                </Typography>
-              </Grid>
-              <hr style={{ color: "white", width: "100%" }} />
-            </Grid>
+            </Card>
           </>
         ))}
       </Container>
