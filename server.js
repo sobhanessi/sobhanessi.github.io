a6d9402ea85086404e8d485c96a7d2a7eca303ed
sobhanessi <sobhanessi@gmail.com>
2022-08-06 15:18:13 +0200

    starting to work on typescript part and then I will move to graphql or maybe both at the same time, idk

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const next = require("next");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./database/schema");
+
+const PORT = process.env.PORT || 3000;
 
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
+const root = { hello: () => "Hi! this is Sobhan!" };
+
 app.prepare().then(() => {
   const server = express();
+
+  server.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+      rootValue: root,
+    })
+  );
 
   server.get("/news", (req, res) => {
     return app.render(req, res, "/news");
@@ -19,5 +34,5 @@ app.prepare().then(() => {
     handle(req, res);
   });
 
-  server.listen(3000, () => console.log("app is published on the port 3000"));
+  server.listen(PORT, () => console.log("app is published on the port 3000"));
 });
