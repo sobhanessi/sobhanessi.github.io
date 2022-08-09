b1c69be409d2a2ab76e00f0f92b2076f98e73d23
sobhanessi <sobhanessi@gmail.com>
2022-08-09 22:20:04 +0200

    making it ready to be published.

---
+++ .next/server/chunks/739.js
@@ -0,0 +1,92 @@
+"use strict";
+exports.id = 739;
+exports.ids = [739];
+exports.modules = {
+
+/***/ 5739:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5692);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_mui_material__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _public_theme_theme__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(3018);
+/* harmony import */ var _mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(9254);
+/* harmony import */ var _mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(5939);
+/* harmony import */ var _mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
+
+
+
+function Footer() {
+    const year = new Date().getUTCFullYear();
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Divider, {}),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Container, {
+                sx: {
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: _public_theme_theme__WEBPACK_IMPORTED_MODULE_3__/* .MAIN_COLOR */ .lU,
+                    bottom: 0,
+                    pt: 2
+                },
+                maxWidth: "xl",
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Typography, {
+                        color: "white",
+                        gutterBottom: true,
+                        sx: {
+                            fontFamily: _public_theme_theme__WEBPACK_IMPORTED_MODULE_3__/* .FONT_FAMILY */ .ut,
+                            fontWeight: "bold"
+                        },
+                        children: [
+                            "SOBHAN ESFANDYARI | SOBHANESSI | DAWSHESSI 2018 - ",
+                            year
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Box, {
+                        sx: {
+                            mb: 1
+                        },
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Typography, {
+                                component: "a",
+                                href: "https://linkedin.com/in/sobhanesfandyari/",
+                                color: "white",
+                                sx: {
+                                    mr: 1
+                                },
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_5___default()), {})
+                            }, "1"),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_2__.Typography, {
+                                component: "a",
+                                href: "https://github.com/sobhanessi",
+                                color: "white",
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((_mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_4___default()), {})
+                            }, "2")
+                        ]
+                    })
+                ]
+            })
+        ]
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+
+/***/ })
+
+};
+;
