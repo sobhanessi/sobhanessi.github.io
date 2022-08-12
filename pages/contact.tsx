a397a4d0822310a02367f9454568c38fabde82e7
sobhanessi <sobhanessi@gmail.com>
2022-08-12 11:07:45 +0200

    nothing

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container, Typography } from "@mui/material";
+import { FONT_FAMILY, MAIN_COLOR } from "../public/theme/theme";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +8,33 @@ export default function contact(): JSX.Element {
   return (
     <>
       <Navbar />
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: MAIN_COLOR,
+          fontFamily: FONT_FAMILY,
+          width: "100%",
+          pt: 10,
+          pb: 10,
+        }}
+      >
+        <Typography
+          pt={10}
+          gutterBottom
+          sx={{
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontFamily: FONT_FAMILY,
+            fontWeight: "bold",
+          }}
+        >
+          this page is under construction.
+        </Typography>
+      </Container>
       <Footer />
     </>
   );
