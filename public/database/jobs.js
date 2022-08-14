c20c2f2eb650b908925d8e1ac28e0bf40f2f6869
sobhanessi <sobhanessi@gmail.com>
2022-08-14 11:25:15 +0200

    changes in the skill badges, now is better!

--- public/database/jobs.js
+++ public/database/jobs.js
@@ -8,18 +8,54 @@ const jobs = [
       "I started my new journey in a German company's branch in Greece as Software Engineer to develope and maintain some applications and web applications using variety of languages for this company.",
     image: "covis.png",
     skills: [
-      { name: "", pic: "/pics/javascript logo.png" },
-      { name: "", pic: "/pics/typescript logo.png" },
-      { name: "", pic: "/pics/nodejs logo 2.png" },
-      { name: "", pic: "/pics/docker logo 2.png" },
-      { name: "", pic: "/pics/mysql logo 2.png" },
-      { name: "mongodb", pic: "/pics/mongodb logo 2.png" },
-      { name: "React", pic: "/pics/react logo 2.png" },
-      // { name: "React Native", pic: "/pics/react logo 2.png" },
-      { name: "redux", pic: "/pics/redux logo 2.png" },
-      { name: "", pic: "/pics/graphql logo 2.png" },
-      { name: "", pic: "/pics/python logo 2.png" },
-      { name: "git", pic: "/pics/git logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/react_native-61DAFB.svg?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white)",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white",
+      },
     ],
   },
   {
@@ -31,13 +67,34 @@ const jobs = [
       "English instructor for Farsi speakers in the level of A0 to A2. I also taught here ECDL and CLC but my main focus in teaching was teaching Full Stack Development with Javascript, NodeJS, React, HTML, CSS, MySql, MongoDB and so many other technologies beside my teaching like 'How To Think Like A Programmer' and 'How To Solve Problems'. I have had almost 10 students for each my classes.",
     image: "pytheas path.png",
     skills: [
-      { name: "", pic: "/pics/javascript logo.png" },
-      { name: "nodejs", pic: "/pics/nodejs logo 2.png" },
-      { name: "", pic: "/pics/mysql logo 2.png" },
-      { name: "mongo", pic: "/pics/mongodb logo 2.png" },
-      { name: "React", pic: "/pics/react logo 2.png" },
-      { name: "", pic: "/pics/python logo 2.png" },
-      { name: "git", pic: "/pics/git logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white",
+      },
     ],
   },
   {
@@ -49,11 +106,23 @@ const jobs = [
       "I worked in Solidarity Now as a volunteer IT developer and supporter to help them to manage their services.",
     image: "SolidarityNow.jpg",
     skills: [
-      { name: "", pic: "/pics/microsoft windows logo.jpg" },
-      { name: "", pic: "/pics/python logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+      },
 
-      { name: "", pic: "/pics/cisco logo.jpg" },
-      { name: "linux", pic: "/pics/linux logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Cisco-1BA0D7.svg?style=for-the-badge&logo=Cisco&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black",
+      },
     ],
   },
   {
@@ -65,10 +134,22 @@ const jobs = [
       "I worked here as IT manager. I was responsible for more than 12 computers including designing the core network and installing and maintaining and optimizing systems for the company.",
     image: "part shamim daru.png",
     skills: [
-      { name: "", pic: "/pics/microsoft windows logo.jpg" },
-      { name: "WordPress", pic: "/pics/wordpress logo.png" },
-      { name: "linux", pic: "/pics/linux logo 2.png" },
-      { name: "", pic: "/pics/javascript logo.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/WordPress-%23117AC9.svg?style=for-the-badge&logo=WordPress&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black",
+      },
     ],
   },
   {
@@ -89,9 +170,18 @@ const jobs = [
       "I was responsible for entire warehouse which for a retail company with outcome of more than 10 tons per month it is more than usual which I digitalised the warehouse system from classic to modern.",
     image: "part shamim daru.png",
     skills: [
-      { name: "", pic: "/pics/mysql logo 2.png" },
-      { name: "React", pic: "/pics/react logo 2.png" },
-      { name: "", pic: "/pics/graphql logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white)",
+      },
     ],
   },
   {
@@ -112,9 +202,18 @@ const jobs = [
       "Bagro was fast food chain restaurant which was located in Tehran and I was responsible for their branches' computers and systems including installing and maintaining. Bagro had more than 4 branches throughout Tehran.",
     image: "bagro fast food.jpg",
     skills: [
-      { name: "", pic: "/pics/microsoft windows logo.jpg" },
-      { name: "linux", pic: "/pics/linux logo 2.png" },
-      { name: "", pic: "/pics/cisco logo.jpg" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Cisco-1BA0D7.svg?style=for-the-badge&logo=Cisco&logoColor=white",
+      },
     ],
   },
   {
@@ -126,10 +225,19 @@ const jobs = [
       "Worked as help desk technician. And this job was my first full time job. I was responsible for Kian software which was a software for managing their branch's warehouse and sales management.",
     image: "faraz aftab caspian.jpg",
     skills: [
-      { name: "", pic: "/pics/microsoft windows logo.jpg" },
-      { name: "linux", pic: "/pics/linux logo 2.png" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white",
+      },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black",
+      },
       // { name: "Kiyan Software", pic: "" },
-      { name: "", pic: "/pics/cisco logo.jpg" },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/Cisco-1BA0D7.svg?style=for-the-badge&logo=Cisco&logoColor=white",
+      },
     ],
   },
 ];
