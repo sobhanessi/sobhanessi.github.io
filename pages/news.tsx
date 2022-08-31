c2887e62e082fba1ae23deb23d6e0ed405c8e09b
sobhanessi <sobhan@esfandyari.org>
2022-08-31 14:42:29 +0200

    some changes from container to grid

--- pages/news.tsx
+++ pages/news.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container } from "@mui/material";
-import { FONT_FAMILY, MAIN_COLOR } from "../public/theme/theme";
+import { Grid } from "@mui/material";
+import { MAIN_COLOR } from "../public/theme/theme";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import NewsTiles from "./components/NewsTiles";
@@ -11,19 +11,17 @@ function news() {
   return (
     <>
       <Navbar />
-      <Container
-        maxWidth="xl"
+      <Grid
+        container
         sx={{
           display: "flex",
-          flexDirection: "column",
           backgroundColor: MAIN_COLOR,
-          fontFamily: FONT_FAMILY,
           width: "100%",
           pt: 10,
         }}
       >
         <NewsTiles />
-      </Container>
+      </Grid>
       <Footer />
     </>
   );
