0aa9c2fa52eefde667558cbacb3f6cda2d83aa1d
sobhanessi <sobhan@esfandyari.org>
2022-08-31 15:09:45 +0200

    regarding last commit

---
+++ .next/server/chunks/656.js
@@ -0,0 +1,80 @@
+"use strict";
+exports.id = 656;
+exports.ids = [656];
+exports.modules = {
+
+/***/ 2656:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5692);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_mui_material__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _public_theme_theme__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4241);
+/* harmony import */ var _mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(9254);
+/* harmony import */ var _mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5939);
+/* harmony import */ var _mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+
+function Footer() {
+  const year = new Date().getUTCFullYear();
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Divider, null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Grid, {
+    sx: {
+      backgroundColor: _public_theme_theme__WEBPACK_IMPORTED_MODULE_2__/* .MAIN_COLOR */ .lU,
+      bottom: 0,
+      pt: 2,
+      width: "100%"
+    },
+    container: true
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Grid, {
+    item: true,
+    sx: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "80%",
+      ml: "10%"
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Typography, {
+    color: "white",
+    gutterBottom: true,
+    sx: {
+      fontFamily: _public_theme_theme__WEBPACK_IMPORTED_MODULE_2__/* .FONT_FAMILY */ .ut,
+      fontWeight: "bold"
+    }
+  }, "SOBHAN ESFANDYARI | SOBHANESSI | DAWSHESSI 2018 - ", year), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Box, {
+    sx: {
+      mb: 1
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Typography, {
+    component: "a",
+    href: "https://linkedin.com/in/sobhanesfandyari/",
+    color: "white",
+    sx: {
+      mr: 1
+    },
+    key: "1"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((_mui_icons_material_LinkedIn__WEBPACK_IMPORTED_MODULE_4___default()), null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__.Typography, {
+    component: "a",
+    href: "https://github.com/sobhanessi",
+    color: "white",
+    key: "2"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((_mui_icons_material_GitHub__WEBPACK_IMPORTED_MODULE_3___default()), null))))));
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+/***/ })
+
+};
+;
