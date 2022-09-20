c2c2a57eaec34381d5c00726f4eef098a756d7f8
sobhanessi <sobhan@esfandyari.org>
2022-09-20 15:50:35 +0200

    added a new job

--- public/database/jobs.js
+++ public/database/jobs.js
@@ -1,6 +1,69 @@
 const jobs = [
   {
     title: "Software Engineer",
+    company: "OnePoint Ltd.",
+    contractType: "Contract",
+    period: "7/2022 - Present",
+    description:
+      "I work in this company as a software engineer in variety of fields and technologies mostly related to Dell Boomi.",
+    image: "onepoint.jpeg",
+    skills: [
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black",
+      },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
+      // },
+      {
+        name: "",
+        pic: "https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white",
+      },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/react_native-61DAFB.svg?style=for-the-badge&logo=react&logoColor=black",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white)",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+      // },
+      // {
+      //   name: "",
+      //   pic: "https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white",
+      // },
+      {
+        name: "",
+        pic: "/pics/boomi.jpg",
+      },
+    ],
+  },
+  {
+    title: "Software Engineer",
     company: "Dr. Glinz Covis GmbH",
     contractType: "Full Time",
     period: "6/2021 - 6/2022 | 1 year",
