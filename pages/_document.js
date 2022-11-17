3ecc28a0ea47501a2cd9772a3113898e57a0c6d9
sobhanessi <sobhan@esfandyari.org>
2022-11-17 19:19:15 +0100

    fixed the issue with next js hydration

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,85 @@
-import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+// import React from "react";
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+// import { ServerStyleSheet } from "styled-components";
+// import { ServerStyleSheets } from "@mui/styles";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link
-          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </Head>
-      {/* persian fonts */}
-      <link rel="stylesheet" href="//unpkg.com/@raha.group/persian-fonts" />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// class MyDocument extends Document {
+//   render(): JSX.Element {
+//     return (
+
+//     );
+//   }
+// }
+
+// MyDocument.getInitialProps = async (ctx) => {
+//   // Render app and page and get the context of the page with collected side effects.
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
+
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+//     });
+
+//   const initialProps = await Document.getInitialProps(ctx);
+
+//   return {
+//     ...initialProps,
+//     // Styles fragment is rendered after the app and page rendering finish.
+//     styles: [
+//       ...React.Children.toArray(initialProps.styles),
+//       sheets.getStyleElement(),
+//     ],
+//   };
+// };
+
+// export default MyDocument;
+
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+// import { ServerStyleSheet } from "styled-components";
+// import { ServerStyleSheets } from "@mui/styles";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </Head>
+        {/* persian fonts */}
+        <link rel="stylesheet" href="//unpkg.com/@raha.group/persian-fonts" />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// MyDocument.getInitialProps = async (ctx) => {
+//   // Render app and page and get the context of the page with collected side effects.
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
+
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+//     });
+
+//   const initialProps = await Document.getInitialProps(ctx);
+
+//   return {
+//     ...initialProps,
+//     // Styles fragment is rendered after the app and page rendering finish.
+//     styles: [
+//       ...React.Children.toArray(initialProps.styles),
+//       sheets.getStyleElement(),
+//     ],
+//   };
+// };
