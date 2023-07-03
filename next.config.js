5a029282c5c2ab8c81ced95d5be0315240c338da
sobhanessi <sobhan@esfandyari.com>
2023-07-03 15:32:12 +0200

    migrated to swc from babel

--- next.config.js
+++ next.config.js
@@ -1,18 +1,50 @@
 // this file tends to work only in windows for working with docker
 module.exports = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  compiler: {
-    styledComponents: true,
-  },
-  //images :
-  images: {
-    loader: "default",
-    domains: ["localhost"],
-  },
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 800,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
+    compiler: {
+        styledComponents: true,
+        removeConsole: {
+            exclude: ["log"],
+        },
+    },
+    //images :
+    images: {
+        loader: "default",
+        domains: ["localhost"],
+    },
 };
+
+// module.exports = {
+//   compiler: {
+//     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+//     styledComponents: boolean | {
+//       // Enabled by default in development, disabled in production to reduce file size,
+//       // setting this will override the default for all environments.
+//       displayName?: boolean,
+//       // Enabled by default.
+//       ssr?: boolean,
+//       // Enabled by default.
+//       fileName?: boolean,
+//       // Empty by default.
+//       topLevelImportPaths?: string[],
+//       // Defaults to ["index"].
+//       meaninglessFileNames?: string[],
+//       // Enabled by default.
+//       cssProp?: boolean,
+//       // Empty by default.
+//       namespace?: string,
+//       // Not supported yet.
+//       minify?: boolean,
+//       // Not supported yet.
+//       transpileTemplateLiterals?: boolean,
+//       // Not supported yet.
+//       pure?: boolean,
+//     },
+//   },
+// }
