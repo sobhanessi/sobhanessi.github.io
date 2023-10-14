2a2f14d9578c00063f2605cd55c5d10a522b0f78
sobhanessi <sobhan@esfandyari.com>
2023-10-14 19:15:38 +0200

    nothing

--- public/theme/theme.js
+++ public/theme/theme.js
@@ -8,27 +8,27 @@ export const FONT_FAMILY = "'Source Code Pro', monospace";
 export const PERSIAN_FONT_FAMILY = "Yekan";
 
 export const NAVBAR_ICON_BUTTON_DISPLAY = {
-  xl: "none",
-  lg: "none",
-  md: "none",
-  sm: "none",
+    xl: "none",
+    lg: "none",
+    md: "none",
+    sm: "none",
 };
 
 export const NAVBAR_CONTAINER_DISPLAY = {
-  xs: "none",
-  xl: "flex",
-  lg: "flex",
-  md: "flex",
-  sm: "flex",
+    xs: "none",
+    xl: "flex",
+    lg: "flex",
+    md: "flex",
+    sm: "flex",
 };
 
 export const NAVBAR_APPBAR_TYPOGRAPHY = {
-  mr: 2,
-  fontFamily: "monospace",
-  display: "flex",
-  alignItems: "center",
-  fontWeight: 700,
-  letterSpacing: ".12rem",
-  color: "inherit",
-  textDecoration: "none",
+    mr: 2,
+    fontFamily: "monospace",
+    display: "flex",
+    alignItems: "center",
+    fontWeight: 700,
+    letterSpacing: ".12rem",
+    color: "inherit",
+    textDecoration: "none",
 };
