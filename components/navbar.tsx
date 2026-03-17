6073e598ef8368f12382968b9f8ac56486545d44
sobhanessi <sobhan@esfandyari.com>
2026-03-17 08:38:01 +0100

    some cleanups in navbar.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,6 @@ import {
     NavbarItem,
     NavbarMenuItem,
 } from "@heroui/navbar";
-// import { Button } from "@heroui/button";
 import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
@@ -91,21 +90,6 @@ export const Navbar = () => {
                     {searchInput}
                 </NavbarItem>
                 <NavbarMenuToggle className="lg:hidden" />
-
-                {/* <NavbarItem className="hidden md:flex">
-                    <Button
-                        isExternal
-                        as={Link}
-                        className="text-sm font-normal text-default-600 bg-default-100"
-                        href={siteConfig.links.sponsor}
-                        startContent={
-                            <HeartFilledIcon className="text-danger" />
-                        }
-                        variant="flat"
-                    >
-                        Sponsor
-                    </Button>
-                </NavbarItem> */}
             </NavbarContent>
 
             <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
