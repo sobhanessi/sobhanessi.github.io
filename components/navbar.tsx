b9e5c3943af585c04d95c8a3ff95d7b6ae9ad628
sobhanessi <sobhanessi@gmail.com>
2026-03-18 11:47:23 +0100

    some improvements in the navbar links.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
     Navbar as HeroUINavbar,
     NavbarContent,
@@ -17,8 +18,13 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { GithubIcon, SearchIcon, Logo, LinkedInIcon } from "@/components/icons";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
+    const pathname = usePathname();
+
+    console.log(pathname);
+
     const searchInput = (
         <Input
             aria-label="Search"
@@ -57,16 +63,16 @@ export const Navbar = () => {
                 <ul className="hidden lg:flex gap-4 justify-start ml-2">
                     {siteConfig.navItems.map((item) => (
                         <NavbarItem key={item.href}>
-                            <NextLink
-                                className={clsx(
-                                    linkStyles({ color: "foreground" }),
-                                    "data-[active=true]:text-primary data-[active=true]:font-medium",
-                                )}
-                                color="foreground"
+                            <Link
+                                color={
+                                    item.href === pathname
+                                        ? "primary"
+                                        : "foreground"
+                                }
                                 href={item.href}
                             >
                                 {item.label}
-                            </NextLink>
+                            </Link>
                         </NavbarItem>
                     ))}
                 </ul>
@@ -125,14 +131,11 @@ export const Navbar = () => {
                         <NavbarMenuItem key={`${item}-${index}`}>
                             <Link
                                 color={
-                                    index === 2
+                                    item.href === pathname
                                         ? "primary"
-                                        : index ===
-                                            siteConfig.navItems.length - 1
-                                          ? "danger"
-                                          : "foreground"
+                                        : "foreground"
                                 }
-                                href="#"
+                                href={item.href}
                                 size="lg"
                             >
                                 {item.label}
