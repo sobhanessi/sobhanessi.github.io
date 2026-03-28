c0c2ca8cf598a05d4eec5b3164fa044f44006b7f
sobhanessi <sobhanessi@gmail.com>
2026-03-28 08:11:04 +0100

    ran lint.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,62 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
+import { NextIntlClientProvider } from "next-intl";
 
 import { Providers } from "./providers";
-import { NextIntlClientProvider } from "next-intl";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
-    title: {
-        default: siteConfig.name,
-        template: `%s - ${siteConfig.name}`,
-    },
-    description: siteConfig.description,
-    icons: {
-        icon: "/favicon.ico",
-    },
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export const viewport: Viewport = {
-    themeColor: [
-        { media: "(prefers-color-scheme: light)", color: "white" },
-        { media: "(prefers-color-scheme: dark)", color: "black" },
-    ],
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html suppressHydrationWarning lang="en">
-            <head />
-            <body
-                className={clsx(
-                    "min-h-screen text-foreground bg-background font-sans antialiased",
-                    fontSans.variable,
-                )}
-            >
-                <NextIntlClientProvider>
-                    <Providers
-                        themeProps={{
-                            attribute: "class",
-                            defaultTheme: "dark",
-                        }}
-                    >
-                        <div className="relative flex flex-col h-screen">
-                            <Navbar />
-                            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-                                {children}
-                            </main>
-                        </div>
-                    </Providers>
-                </NextIntlClientProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <NextIntlClientProvider>
+          <Providers
+            themeProps={{
+              attribute: "class",
+              defaultTheme: "dark",
+            }}
+          >
+            <div className="relative flex flex-col h-screen">
+              <Navbar />
+              <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+                {children}
+              </main>
+            </div>
+          </Providers>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
