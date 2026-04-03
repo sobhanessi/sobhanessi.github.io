6b4d2a19cd70e8d545c5d81fdbab4e446a74e629
sobhanessi <sobhanessi@gmail.com>
2026-04-03 21:18:18 +0200

    added my logo to the website. I can still make it better. but perhaps a little bit later but not for now.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,120 +1,149 @@
 "use client";
 import {
-  Navbar as HeroUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
+    Navbar as HeroUINavbar,
+    NavbarContent,
+    NavbarMenu,
+    NavbarMenuToggle,
+    NavbarBrand,
+    NavbarItem,
+    NavbarMenuItem,
 } from "@heroui/navbar";
+import { GithubIcon, SearchIcon, Logo, LinkedInIcon } from "@/components/icons";
+import { Input } from "@heroui/input";
+import Image from "next/image";
 import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
-// import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
-// import clsx from "clsx";
-
-import { usePathname } from "next/navigation";
-
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import { GithubIcon, SearchIcon, Logo, LinkedInIcon } from "@/components/icons";
+import { usePathname } from "next/navigation";
+
+import logo from "../static/logo.png";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+    const pathname = usePathname();
+    const searchInput = (
+        <Input
+            aria-label="Search"
+            classNames={{
+                inputWrapper: "bg-default-100",
+                input: "text-sm",
+            }}
+            endContent={
+                <Kbd className="hidden lg:inline-block" keys={["command"]}>
+                    K
+                </Kbd>
+            }
+            labelPlacement="outside"
+            placeholder="Search..."
+            startContent={
+                <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+            }
+            type="search"
+        />
+    );
 
-  return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">Sobhan Esfandyari</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                color={item.href === pathname ? "primary" : "foreground"}
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+    return (
+        <HeroUINavbar maxWidth="xl" position="sticky">
+            <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+                <NavbarBrand as="li" className="gap-3 max-w-fit">
+                    <NextLink
+                        className="flex justify-start items-center gap-1"
+                        href="/"
+                    >
+                        <Image
+                            src={logo}
+                            width={256}
+                            height={256}
+                            alt="Sobhan Esfandyari Logo"
+                            // className="pt-2"
+                        />
+                    </NextLink>
+                </NavbarBrand>
+                <ul className="hidden lg:flex gap-4 justify-start ml-2">
+                    {siteConfig.navItems.map((item) => (
+                        <NavbarItem key={item.href}>
+                            <Link
+                                color={
+                                    item.href === pathname
+                                        ? "primary"
+                                        : "foreground"
+                                }
+                                href={item.href}
+                            >
+                                {item.label}
+                            </Link>
+                        </NavbarItem>
+                    ))}
+                </ul>
+            </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <Link
-            isExternal
-            aria-label="LinkedIn"
-            href={siteConfig.links.linkedIn}
-          >
-            <LinkedInIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarMenuToggle className="lg:hidden" />
-      </NavbarContent>
+            <NavbarContent
+                className="hidden sm:flex basis-1/5 sm:basis-full"
+                justify="end"
+            >
+                <NavbarItem className="hidden sm:flex gap-2">
+                    <Link
+                        isExternal
+                        aria-label="Github"
+                        href={siteConfig.links.github}
+                    >
+                        <GithubIcon className="text-default-500" />
+                    </Link>
+                    <Link
+                        isExternal
+                        aria-label="LinkedIn"
+                        href={siteConfig.links.linkedIn}
+                    >
+                        <LinkedInIcon className="text-default-500" />
+                    </Link>
+                    <ThemeSwitch />
+                </NavbarItem>
+                <NavbarItem className="hidden lg:flex">
+                    {searchInput}
+                </NavbarItem>
+                <NavbarMenuToggle className="lg:hidden" />
+            </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4 " justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <Link isExternal aria-label="LinkedIn" href={siteConfig.links.linkedIn}>
-          <LinkedInIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
+            <NavbarContent className="sm:hidden basis-1 pl-4 " justify="end">
+                <Link
+                    isExternal
+                    aria-label="Github"
+                    href={siteConfig.links.github}
+                >
+                    <GithubIcon className="text-default-500" />
+                </Link>
+                <Link
+                    isExternal
+                    aria-label="LinkedIn"
+                    href={siteConfig.links.linkedIn}
+                >
+                    <LinkedInIcon className="text-default-500" />
+                </Link>
+                <ThemeSwitch />
+                <NavbarMenuToggle />
+            </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={item.href === pathname ? "primary" : "foreground"}
-                href={item.href}
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </HeroUINavbar>
-  );
+            <NavbarMenu>
+                {searchInput}
+                <div className="mx-4 mt-2 flex flex-col gap-2">
+                    {siteConfig.navItems.map((item, index) => (
+                        <NavbarMenuItem key={`${item}-${index}`}>
+                            <Link
+                                color={
+                                    item.href === pathname
+                                        ? "primary"
+                                        : "foreground"
+                                }
+                                href={item.href}
+                                size="lg"
+                            >
+                                {item.label}
+                            </Link>
+                        </NavbarMenuItem>
+                    ))}
+                </div>
+            </NavbarMenu>
+        </HeroUINavbar>
+    );
 };
