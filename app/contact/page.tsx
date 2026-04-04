81ddbf4df0c02d93f5fc1bb89c3d331cce526eb6
sobhanessi <sobhanessi@gmail.com>
2026-04-04 09:56:30 +0200

    must add two other pages which were missed from last time that I unfortunately forgot them. Contact page and a Portfolio would make my website more cleaner and more accessible to other people.

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className={title()}>Contact</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function PortfolioPage() {
+  return (
+    <div>
+      <h1 className={title()}>Portfolio</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
